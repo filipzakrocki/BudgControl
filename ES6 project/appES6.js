6be4b0cc679219b48add14efe843aa2c7cb70dcb
filipzakrocki <42967255+filipzakrocki@users.noreply.github.com>
2019-03-02 12:14:28 +0000

    Update appES6.js
    removed comments from es5

--- ES6 project/appES6.js
+++ ES6 project/appES6.js
@@ -1,129 +1,3 @@
-//// BUDGET CONTROLLER
-//var budgetController = (function() {
-//  var Expense = function(id, description, value) {
-//    this.id = id;
-//    this.description = description;
-//    this.value = value;
-//    this.percentage = -1;
-//  };
-//
-//  Expense.prototype.calcPercentage = function(totalIncome) {
-//    if (totalIncome > 0) {
-//      this.percentage = Math.round(this.value / totalIncome * 100);
-//    } else {
-//      this.percentage = -1;
-//    }
-//  };
-//
-//  Expense.prototype.getPercentage = function() {
-//    return this.percentage;
-//  };
-//
-//  var Income = function(id, description, value) {
-//    this.id = id;
-//    this.description = description;
-//    this.value = value;
-//  };
-//
-//  var calculateTotal = function(type) {
-//    var sum = 0;
-//    data.allItems[type].forEach(function(cur) {
-//      sum += cur.value;
-//    });
-//    data.totals[type] = sum;
-//  };
-//
-//  var data = {
-//    allItems: {
-//      exp: [],
-//      inc: []
-//    },
-//    totals: {
-//      exp: 0,
-//      inc: 0
-//    },
-//    budget: 0,
-//    percentage: -1
-//  };
-//
-//  return {
-//    addItem: function(type, des, val) {
-//      var newItem, ID;
-//
-//      if (data.allItems[type].length > 0) {
-//        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-//      } else {
-//        ID = 0;
-//      }
-//
-//      if (type === "exp") {
-//        newItem = new Expense(ID, des, val);
-//      } else if (type === "inc") {
-//        newItem = new Income(ID, des, val);
-//      }
-//
-//      data.allItems[type].push(newItem);
-//
-//      return newItem;
-//    },
-//
-//    deleteItem: function(type, id) {
-//      var ids, index;
-//
-//      ids = data.allItems[type].map(function(current) {
-//        return current.id;
-//      });
-//
-//      index = ids.indexOf(id);
-//
-//      if (index !== -1) {
-//        data.allItems[type].splice(index, 1);
-//      }
-//    },
-//
-//    calculateBudget: function() {
-//      calculateTotal("exp");
-//      calculateTotal("inc");
-//
-//      data.budget = data.totals.inc - data.totals.exp;
-//
-//      if (data.totals.inc > 0) {
-//        data.percentage = Math.round(data.totals.exp / data.totals.inc * 100);
-//      } else {
-//        data.percentage = -1;
-//      }
-//    },
-//
-//    calculatePercentages: function() {
-//      data.allItems.exp.forEach(function(cur) {
-//        cur.calcPercentage(data.totals.inc);
-//      });
-//    },
-//
-//    getPercentages: function() {
-//      var allPerc = data.allItems.exp.map(function(cur) {
-//        return cur.getPercentage();
-//      });
-//      return allPerc;
-//    },
-//
-//    getBudget: function() {
-//      return {
-//        budget: data.budget,
-//        totalInc: data.totals.inc,
-//        totalExp: data.totals.exp,
-//        percentage: data.percentage
-//      };
-//    },
-//
-//    testing: function() {
-//      console.log(data);
-//    }
-//  };
-//})();
-
-
-
 //iife
 // BUDGET CONTROLLER}
 let budgetController = (function () {
@@ -262,18 +136,6 @@ let budgetController = (function () {
 })();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // UI CONTROLLER
 const UIController = (function() {
     
@@ -321,7 +183,7 @@ const UIController = (function() {
       
       //Returned functions
       
-    getInput: function() {
+    getInput() {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
@@ -329,7 +191,7 @@ const UIController = (function() {
       };
     },
 
-    addListItem: function(obj, type) {
+    addListItem(obj, type) {
       let html, newHtml, element;
         
       if (type === "inc") {
@@ -351,7 +213,7 @@ const UIController = (function() {
         
     },
 
-    deleteListItem: function(selectorID) {
+    deleteListItem(selectorID) {
       let el = document.getElementById(selectorID);
       el.parentNode.removeChild(el);
     },
